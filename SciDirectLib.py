39ea1c17281be93211921b5dabb42391abde2d03
Jim Kadin <james.kadin@jax.org>
2021-05-10 13:01:56 -0400

    implement initial SciDirectReference class & tests

--- SciDirectLib.py
+++ SciDirectLib.py
@@ -1,8 +1,35 @@
 """A Python module that provides classes to talk to the Elsevier ScienceDirect
-   API for searching for references and downloading meta-data and PDFs
-   API documentation (JIM: is this right?)
-   * https://dev.elsevier.com
-   * https://api.elsevier.com
+    API for searching for references and downloading metadata and PDFs
+
+    This is based on an open source python client provided by Elsevier at
+    https://github.com/ElsevierDev/elsapy
+
+    BUT has been much stripped down and simplified for MGI's purposes and
+    modified to support:
+    (1) use of the SciDirect PUT API interface instead of the old, depricated
+       GET interface
+    (2) downloading of PDFs
+    (elsapy also does not seem to be supported much):
+
+    Documentation for the API itself (not elsapy)
+    * The PUT API takes a json payload to specify the query parameters and
+        returns a json result set.
+
+    * Best docs for this json exchange that I've found:
+        https://dev.elsevier.com/tecdoc_sdsearch_migration.html
+            (but it is a little confusing because it is couched in terms of
+            the old GET API)
+
+    * more API docs, but doesn't explain the json payloads:
+        https://dev.elsevier.com/documentation/ScienceDirectSearchAPI.wadl
+
+    * overview of different Elsevier APIs - we are most concerned with
+        ScienceDirect which includes full text search.
+        (Scopus only supports abstracts)
+        https://dev.elsevier.com/support.html
+
+    * interactively play with the API here:
+        https://dev.elsevier.com/sciencedirect.html#/
 
 Class Overview
     class ElsClient
@@ -13,7 +40,7 @@ Class Overview
     - exec_requestGet(url, contentType) executes a GET request with
         result content-type either json or pdf
     - exec_requestPut(url, params) executes a PUT request after coding the
-        params in json. Returns json result.
+        params in json. Returns unserialized json result.
 
     class SciDirectSearch
     - Does a search against the SciDirect API and provides access to the search
@@ -31,6 +58,10 @@ Class Overview
     - has article metadata: reference IDs, Journal, title, abstract, full text,
         pdf, etc.
     - lazily makes requests to the API to get additional metadata/pdf
+
+There are automated tests for this module:
+    cd tests
+    python test_SciDirectLib.py [-v]
 """
 
 import requests, json, time, os, logging
@@ -68,7 +99,7 @@ def get_logger(name):
 logger = get_logger(__name__)
 url_base = "https://api.elsevier.com/"
 
-class ElsClient:
+class ElsClient(object):
     """A class that implements a Python interface to api.elsevier.com"""
 
     __user_agent = "MGI-SciDirectClient"
@@ -192,3 +223,91 @@ class ElsClient:
     	return {'status_code':self._status_code, 'status_msg': self._status_msg}
 # end class ElsClient -------------------------
 
+class SciDirectReference(object):
+    """
+    IS:   a reference at ScienceDirect.
+    HAS:  IDs, basic metadata fields: title, journal, dates, ...
+    DOES: loads metadata lazily. Gets PDF.
+    """
+    def __init__(self, elsClient, data=None):
+        """ Instantiate a reference object.
+            data = record / dict from the SciDirectSearch results from the API
+        """
+        self.elsClient = elsClient
+
+        # unpack fields from SciDirectSearch results
+        self.searchResultsFields = data
+        self._unpackSciDirectResult()
+
+        # fields we have to load from a ref details API call
+        self.detailFields = None      # the results from the details API call
+        self.pmid = None
+        self.pubType = None
+        self.abstract = None
+
+        # the binary pdf contents are loaded from a subsequent API call
+        self.pdf = None
+
+    def _unpackSciDirectResult(self):
+        """ unpack the dict from SciDirectSearch result representing the ref
+        """
+        self.pii             = self.searchResultsFields['pii']
+        self.doi             = self.searchResultsFields['doi']
+        self.journal         = self.searchResultsFields['sourceTitle']
+        self.title           = self.searchResultsFields['title']
+        self.volumeIssue     = self.searchResultsFields['volumeIssue']
+        self.loadDate        = self.searchResultsFields['loadDate']
+        self.publicationDate = self.searchResultsFields['publicationDate']
+
+    # getters for fields from SciDirectSearch result
+    def getPii(self):         return self.pii
+    def getDoi(self):         return self.doi
+    def getJournal(self):     return self.journal
+    def getTitle(self):       return self.title
+    def getVolumeIssue(self): return self.volumeIssue
+    def getLoadDate(self):    return self.loadDate
+    def getPublicationDate(self): return self.publicationDate
+
+    # getters for fields from ref details API call
+    def getPmid(self):
+        self._getDetails()
+        return self.pmid
+    def getPubType(self):
+        self._getDetails()
+        return self.pubType
+    def getAbstract(self):
+        self._getDetails()
+        return self.abstract
+
+    def _getDetails(self):
+        """ load the reference details from the API if they have not already
+            been loaded.
+        """
+        if not self.detailFields:
+            url = url_base + 'content/article/pii/' + str(self.pii)
+            response = self.elsClient.execGetRequest(url)
+            # TODO: should we dump json output somewhere for debugging?
+            r = response['full-text-retrieval-response']
+            self.detailFields = r
+
+            # unpack the fields, just these for now. Other fields are avail
+            if 'pubmed-id' in r:
+                self.pmid = r['pubmed-id']
+            else:
+                self.pmid = "no PMID"       # not all articles have pmid yet
+            self.pubType  = r['coredata']['pubType']
+            self.abstract = r['coredata']['dc:description']
+
+    # getters for the PDF
+    def getPdf(self):
+        self._getPdf()
+        return self.pdf
+
+    def _getPdf(self):
+        """ Get the PDF from the API if we have not already done so
+        """
+        if not self.pdf:
+            url = url_base + 'content/article/pii/' + str(self.pii)
+            self.pdf = self.elsClient.execGetRequest(url, contentType='pdf')
+
+# end class SciDirectReference -------------------------
--- test/test_SciDirectLib.py
+++ test/test_SciDirectLib.py
@@ -49,8 +49,63 @@ class ElsClient_tests(unittest.TestCase):
 
     # test_execPutRequest
 
-# end class ElsClient_tests
-######################################
+# end class ElsClient_tests ######################################
+
+class SciDirectReference_tests(unittest.TestCase):
+    ref1Data = {      # taken from SciDirect search results. PMID 33417945
+        "authors": [
+          {
+            "name": "Hongqiao Zhang",
+            "order": 1
+          },
+          {
+            "name": "Todd E. Morgan",
+            "order": 2
+          },
+          {
+            "name": "Henry Jay Forman",
+            "order": 3
+          }
+        ],
+        "doi": "10.1016/j.abb.2020.108749",
+        "loadDate": "2021-01-05T00:00:00.000Z",
+        "openAccess": False,
+        "pages": {
+          "first": "108749"
+        },
+        "pii": "S0003986120307578",
+        "publicationDate": "2021-03-15",
+        "sourceTitle": "Archives of Biochemistry and Biophysics",
+        "title": "Age-related alteration in HNE elimination enzymes",
+        "uri": "https://www.sciencedirect.com/science/article/pii/S0003986120307578?dgcid=api_sd_search-api-endpoint",
+        "volumeIssue": "Volume 699"
+        }
+
+    def test_constructor_getters(self):
+        r1 = sdl.SciDirectReference(elsClient, data=self.ref1Data) 
+        self.assertEqual("S0003986120307578", r1.getPii())
+        self.assertEqual("10.1016/j.abb.2020.108749", r1.getDoi())
+        self.assertEqual("Archives of Biochemistry and Biophysics", r1.getJournal())
+        self.assertEqual("Age-related alteration in HNE elimination enzymes", r1.getTitle())
+        self.assertEqual("Volume 699", r1.getVolumeIssue())
+        self.assertEqual("2021-01-05T00:00:00.000Z", r1.getLoadDate())
+        self.assertEqual("2021-03-15", r1.getPublicationDate())
+
+    def test_fetching_details(self):
+        r1 = sdl.SciDirectReference(elsClient, data=self.ref1Data) 
+        self.assertEqual("33417945", r1.getPmid())
+        self.assertEqual("rev", r1.getPubType())
+        self.assertEqual("4-hydroxynonena", r1.getAbstract()[:15])
+
+    def test_fetching_pdf(self):
+        r1 = sdl.SciDirectReference(elsClient, data=self.ref1Data) 
+        self.assertEqual(b'%PDF-1.7', r1.getPdf()[:8])
+        self.assertEqual(1173669, len(r1.getPdf()))
+        #fp = open(r1.getPii() + '.pdf', 'wb')
+        #fp.write(r1.getPdf())
+        #fp.close()
+
+# end class SciDirectReference_tests ######################################
 
 
 if __name__ == '__main__':
